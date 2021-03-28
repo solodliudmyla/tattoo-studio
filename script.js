1a337d8c49b8d4998187f77fd087e8c0561d649f
Solod Liudmyla <solodludmila@gmail.com>
2021-03-29 00:57:08 +0300

    fix: close nav menu after selection

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-var galleryThumbs = new Swiper('.gallery-thumbs', {
+const galleryThumbs = new Swiper('.gallery-thumbs', {
   spaceBetween: 10,
   slidesPerView: 4,
   loop: true,
   freeMode: true,
   loopedSlides: 5, //looped slides should be the same
   watchSlidesVisibility: true,
-  watchSlidesProgress: true,
+  watchSlidesProgress: true
 });
-var galleryTop = new Swiper('.gallery-top', {
+const galleryTop = new Swiper('.gallery-top', {
   spaceBetween: 10,
   loop: true,
   loopedSlides: 5, //looped slides should be the same
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    prevEl: '.swiper-button-prev'
   },
   thumbs: {
-    swiper: galleryThumbs,
-  },
+    swiper: galleryThumbs
+  }
 });
-var video = document.getElementById("myVideo");
+const video = document.getElementById('myVideo');
 video.play();
 
+// close the nav dropdown menu after user selected
+
+const triggerTabList = [].slice.call(document.querySelectorAll('.nav-item a'));
+const triggerNavbar = document.querySelector('#navbarSupportedContent');
+triggerTabList.forEach(function(triggerEl) {
+  triggerEl.addEventListener('click', function(event) {
+    triggerNavbar.classList.remove('show');
+  });
+});
